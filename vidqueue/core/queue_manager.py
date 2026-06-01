6b4269270df86df8743f6f7cdf72c53a099b4ef4
CryptAmarok <chrisredon1@gmail.com>
2026-06-01 14:39:57 +0200

    feat: implement json queue loading

--- vidqueue/core/queue_manager.py
+++ vidqueue/core/queue_manager.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import json
 
+
 STATE_DIR = Path(__file__).resolve().parents[2]
 STATE_FILE = STATE_DIR / ".queue_state.json"
 TMP_FILE = STATE_DIR / ".queue_state.tmp"
@@ -21,7 +22,7 @@ def save_queue_state(video_paths: list[Path], args: list) -> None:
     data = {
         'videos_list': [str(path) for path in queue],
         'output_path': str(output_path),
-        'ffmpeg_settings': arguments
+        'ffmpeg_settings': arguments,
     }
 
     with TMP_FILE.open("w", encoding="utf-8") as f:
@@ -29,12 +30,35 @@ def save_queue_state(video_paths: list[Path], args: list) -> None:
 
     TMP_FILE.replace(STATE_FILE)
 
+
+def load_queue() -> dict | None:
+
+    with STATE_FILE.open("r", encoding="utf-8") as f:
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            return None
+
+    videos_list = [Path(p) for p in data.get('videos_list', [])]
+    output_path = data.get('output_path', None)
+    ffmpeg_settings = data.get('ffmpeg_settings', None)
+
+    if videos_list and output_path and ffmpeg_settings:
+        return {
+            'videos_list': videos_list,
+            'output_path': output_path,
+            'ffmpeg_settings': ffmpeg_settings
+        }
+    else:
+        None
+
+
 def is_empty() -> bool:
-    
+
     # if the file doesn't exist
     if not STATE_FILE.exists():
         return True
-    
+
     with STATE_FILE.open("r", encoding="utf-8") as f:
         try:
             data = json.load(f)
